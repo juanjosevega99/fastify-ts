840678cb69c11bebd9ef06e6ceb35d05b3019ebe
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-08-25 21:31:19 -0500

    chore: create route to add diaries

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,22 @@
 import fastify from 'fastify';
+import * as diaryServices from '../services/diaryServices'
+import toNewDiaryEntry from '../utils';
 
 const server = fastify();
 
-server.get<{}>('/', {}, async (_request, reply) => {
-  reply.send('Fetching');
+server.get('/', {}, async (_request, reply) => {
+  reply.send();
 });
 
-server.post('/', {}, async (_request, reply) => {
-  reply.send('Saving a diary');
+server.post('/', {}, async (request, reply) => {
+  try {
+    const NewDiaryEntry = toNewDiaryEntry(request.body);
+    const addedDiaryEntry = diaryServices.addDiary(NewDiaryEntry);
+
+    reply.send(addedDiaryEntry);
+  } catch (error) {
+    reply.status(400).send(error);
+  }
 });
 
-export default server
+export default server;
