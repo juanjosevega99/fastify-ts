a240e9fe387dd5ca1e521c7fb54886776dd0ec5f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-08-25 21:31:43 -0500

    chore: add service to find diary by id

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -9,18 +9,19 @@ const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
 
 export const getEntries = (): DiaryEntry[] => diaries;
 
-export const findById = (id: number): NonSensitiveInfoDiaryEntry[] => {
-  return diaries.map(({ id, date, weather, visibility }) => {
-    return {
-      id,
-      date,
-      weather,
-      visibility
-    };
-  });
+export const findById = (
+  id: number
+): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  if (entry != null) {
+    const { comment, ...restOfDiary } = entry;
+    return restOfDiary;
+  }
+
+  return undefined;
 };
 
-export const addDiary = (NewDiaryEntry: NewDiaryEntry) => {
+export const addDiary = (NewDiaryEntry: NewDiaryEntry): DiaryEntry => {
   const newDiary = {
     id: Math.max(...diaries.map(d => d.id)) + 1,
     ...NewDiaryEntry
