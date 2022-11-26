e4d585b13518cdf305ad1a41f3a730ccc91d1cf1
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-11-26 14:29:44 -0500

    feat: add function parseComment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,18 @@
 import { NewDiaryEntry } from '../types';
 import { Weather, Visibility } from '../enums';
 
+const isString = (string: string): boolean => {
+  return typeof string === 'string';
+};
+
+const parseComment = (commentFromRequest: any): string => {
+  if (!isString(commentFromRequest)) {
+    throw new Error('Incorrect or missing comment ');
+  }
+
+  return commentFromRequest;
+};
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
     comment: parseComment(object.comment),
